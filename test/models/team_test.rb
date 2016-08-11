2e58732def895d1d117d7cd3785c5e2f3a195608
Michael Becco <e4e6d4d5@gmail.com>
2016-08-10 17:55:15 -0700

    Test Validations on Team Model

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -5,4 +5,28 @@ class TeamTest < ActiveSupport::TestCase
     @team = Team.new(team_name: 'KnowItAlls', participants: ['fred', 'barney',
                      'wilma', 'betty', 'bam bam', 'pebbles'])
   end
+
+  test 'valid team' do
+    assert @team.valid?
+  end
+
+  test 'team participants > 6 invalid' do
+    @team.participants << 'kazoo'
+    assert_not @team.valid?
+  end
+
+  test 'empty team invalid' do
+    @team.participants = []
+    assert_not @team.valid?
+  end
+
+  test 'one member team valid' do
+    @team.participants = ['fred']
+    assert @team.valid?
+  end
+
+  test 'empty team name invalid' do
+    @team.team_name = '   '
+    assert_not @team.valid?
+  end
 end
