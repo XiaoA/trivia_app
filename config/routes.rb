8282d0363de7c03c038d4a4f77fdc569a0b35d2a
Michael Becco <e4e6d4d5@gmail.com>
2016-08-11 14:51:57 -0700

    Add Member #join for Team Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :teams
+  resources :teams do
+    member do
+      patch :join
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
