97e9acf9f8bc7683e32cda0969c4384802519cef
Michael Becco <e4e6d4d5@gmail.com>
2016-08-10 20:59:02 -0700

    Seed Team DB, Test Current Index Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Team.create!(team_name: 'Dubs', participants: ['Curry', 'Thomspson', 'Bogut', 'Green', 'Iggy'])
+Team.create!(team_name: 'Shield', participants: ['Mackey', 'Shane', 'Lem', 'Ronnie'])
--- test/controllers/teams_controller_test.rb
+++ test/controllers/teams_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class TeamsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @team = Team.create!(team_name: 'Test', participants: ['fred'])
+  end
   test "should get index" do
     get teams_path
     assert_response :success
+    assert_select 'div#team_1'
     assert_select "a[href=?]", new_team_path
   end
 
