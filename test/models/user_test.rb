24f36507e16db2c5d79ff5c10465f7738e15cb66
Michael Becco <e4e6d4d5@gmail.com>
2016-08-20 14:02:44 -0700

    Config Devise Password, Test Model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: 'test@example.com', password: 'a' * 128,
+                     username: 'a' * 20)
+  end
+
+  test 'valid user' do
+    assert @user.valid?
+  end
+
+  test 'presence of email' do
+    @user.email = ''
+    refute @user.valid?
+  end
+
+  test 'presence of username' do
+    @user.username = ''
+    refute @user.valid?
+  end
+
+  test 'prsence of password' do
+    @user.password = ''
+    refute @user.valid?
+  end
+
+  test 'username > 21 characters' do
+    @user.username = 'a' * 21
+    refute @user.valid?
+  end
+
+  test 'username < 2 characters' do
+    @user.username = 'a'
+    refute @user.valid?
+  end
+
+  test 'password under 8 characters' do
+    @user.password = 'a' * 7
+    refute @user.valid?
+  end
+
+  test 'password > 128 characters' do
+    @user.password = 'a' * 129
+    refute @user.valid?
+  end
 end
