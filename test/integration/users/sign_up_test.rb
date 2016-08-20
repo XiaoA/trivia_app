7b0ea83c526531c4cda6e788ce428eeea16d2013
Michael Becco <e4e6d4d5@gmail.com>
2016-08-20 14:08:43 -0700

    indentation

--- test/integration/users/sign_up_test.rb
+++ test/integration/users/sign_up_test.rb
@@ -16,8 +16,8 @@ class SignUpTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_difference('User.count', 1) do
       post user_registration_path, params: { user: { email: 'signup@email.com',
-                                       username: 'new_user', password: 'password',
-                                       password_confirmation: 'password' } }
+                                   username: 'new_user', password: 'password',
+                                   password_confirmation: 'password' } }
     end
   end
 
